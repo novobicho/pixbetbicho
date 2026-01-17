79537f8500fa4400312dd4b699414807980d729e
novobicho <grupodobranco@gmail.com>
2026-01-17 10:52:00 -0300

    Add files via upload

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,9 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { initializeTheme } from "./components/theme-provider";
+
+// Inicializar o tema do cache local o mais cedo possível para evitar flicker
+initializeTheme();
 
 createRoot(document.getElementById("root")!).render(<App />);
